c3dc614e2ecb9cfca59c64045bf82d886f4329f6
MallquiDeone <159455849+MallquiDeone@users.noreply.github.com>
2024-05-07 15:22:55 -0500

    projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://mallquidev.github.io',
-  base: 'portfolio-astro',
+  base: '/portfolio-astro',
 });
